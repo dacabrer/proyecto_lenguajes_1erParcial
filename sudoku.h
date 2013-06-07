1d5b210f5e221cea0c8600e852a3910036545711
cramirezmera <cararami@espol.edu.ec>
2013-06-06 23:42:08 -0500

    Version 1.0 Sudoku

---
+++ sudoku.h
@@ -0,0 +1,31 @@
+#ifndef SUDOKU_H
+#define SUDOKU_H
+
+#include <QMainWindow>
+
+namespace Ui {
+class sudoku;
+}
+class QTextEdit;
+
+class sudoku : public QMainWindow
+{
+    Q_OBJECT
+    
+public:
+    explicit sudoku(QWidget *parent = 0);
+    ~sudoku();
+    
+private slots:
+    void setDisplayValue(int i,int j,long v);
+    long getDisplayValue(int i,int j);
+    void on_Validar_clicked();
+
+private:
+    void initGui();
+    Ui::sudoku *ui;
+    QTextEdit *numbertext[9][9];
+};
+
+#endif // SUDOKU_H
+
