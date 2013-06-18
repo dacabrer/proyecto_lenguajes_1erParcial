488377c9b183b7f80497a6b412cb78fa2dead7f6
Jose Velez Gomez <vlezgmezjosantoniomgoo@hotmail.com>
2013-06-18 14:34:46 -0500

    Funcionalidades de las Dificultades
    Niveles de dificultad
    -Validación de ComboBox del Nivel JUVENIL (FACIL)

--- sudoku.h
+++ sudoku.h
@@ -7,7 +7,6 @@ namespace Ui {
 class sudoku;
 }
 class QTextEdit;
-
 class sudoku : public QMainWindow
 {
     Q_OBJECT
@@ -25,10 +24,20 @@ private slots:
 
     void on_pushButton_4_clicked();
 
+    void on_pushButton_3_clicked();
+    void leerArchivo(const QString &nombre_archivo, QString &datos_salida);
+
+    void on_comboBox_activated(const QString &arg1);
+
 private:
     void initGui();
     Ui::sudoku *ui;
     QTextEdit *numbertext[9][9];
+    QTextEdit *pantilla[9][9];
+    const QString nombreArchivo = "D:/Home/Prueba.txt";
+    QString salida;
+    QString nivel;
+
 };
 
 #endif // SUDOKU_H
